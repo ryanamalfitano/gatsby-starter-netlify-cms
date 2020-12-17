b42c3b7daedd3576d35a400610a5e634105a41c7
Ryan Amalfitano <ryan.amalfitano@gmail.com>
2020-12-16 19:51:24 -0500

    Update index-page.js
    Rewrote structure for index-page.js to better fit the new site design.

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,129 +1,84 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
+import { graphql } from 'gatsby'
+import styled from 'styled-components'
 
 import Layout from '../components/Layout'
-import Features from '../components/Features'
-import BlogRoll from '../components/BlogRoll'
+
+const StyledMasthead = styled.div`
+  
+  .stream {
+    background-image: url();
+    background-position: 'top left';
+    background-attachment: 'fixed';
+  }
+
+  .titles {
+
+  }
+`
 
 export const IndexPageTemplate = ({
   image,
   title,
-  heading,
-  subheading,
+  subtitle,
   mainpitch,
-  description,
-  intro,
+  main,
 }) => (
   <div>
-    <div
-      className="full-width-image margin-top-0"
-      style={{
-        backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
-        backgroundPosition: `top left`,
-        backgroundAttachment: `fixed`,
-      }}
-    >
-      <div
-        style={{
-          display: 'flex',
-          height: '150px',
-          lineHeight: '1',
-          justifyContent: 'space-around',
-          alignItems: 'left',
-          flexDirection: 'column',
-        }}
-      >
-        <h1
-          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
+    <StyledMasthead className="columns">
+      <div className="column is-6 titles">
+        <h1 className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen">
           {title}
         </h1>
-        <h3
-          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
-          {subheading}
+        <h3 className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen">
+          {subtitle}
         </h3>
       </div>
-    </div>
-    <section className="section section--gradient">
+      <div className="column is-6 stream full-width-image margin-top-0" style={{backgroundImage: `url(${!!image.childImageSharp ? image.childImageSharp.fluid.src : image})`}}></div>
+    </StyledMasthead>
+    <section className="section">
       <div className="container">
-        <div className="section">
+        <section className="mainPitch">
+          <h1 className="title">{mainpitch.title}</h1>
+          <p className="subtitle">{mainpitch.description}</p>
+        </section>
+        <section className="sellingPoints">
+          <h3 className="has-text-weight-semibold is-size-2">
+            {main.heading}
+          </h3>
+          <p>{main.description}</p>
           <div className="columns">
-            <div className="column is-10 is-offset-1">
-              <div className="content">
-                <div className="content">
-                  <div className="tile">
-                    <h1 className="title">{mainpitch.title}</h1>
-                  </div>
-                  <div className="tile">
-                    <h3 className="subtitle">{mainpitch.description}</h3>
-                  </div>
-                </div>
-                <div className="columns">
-                  <div className="column is-12">
-                    <h3 className="has-text-weight-semibold is-size-2">
-                      {heading}
-                    </h3>
-                    <p>{description}</p>
-                  </div>
-                </div>
-                <Features gridItems={intro.blurbs} />
-                <div className="columns">
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products">
-                      See all products
-                    </Link>
-                  </div>
-                </div>
-                <div className="column is-12">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
-                  </h3>
-                  <BlogRoll />
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/blog">
-                      Read more
-                    </Link>
-                  </div>
-                </div>
-              </div>
+            <div className="column is-4">
+              <img src={!!main.image1.image.childImageSharp ? main.image1.image.childImageSharp.fluid.src : main.image1.image} alt={main.image1.alt} />
+              <p>{main.image1.alt}</p>
+            </div>
+            <div className="column is-4">
+              <img src={!!main.image2.image.childImageSharp ? main.image2.image.childImageSharp.fluid.src : main.image2.image} alt={main.image2.alt} />
+              <p>{main.image2.alt}</p>
+            </div>
+            <div className="column is-4">
+              <img src={!!main.image3.image.childImageSharp ? main.image3.image.childImageSharp.fluid.src : main.image3.image} alt={main.image3.alt} />
+              <p>{main.image3.alt}</p>
             </div>
           </div>
-        </div>
+        </section>
+        <section className="streamsCarousel full-width-image-container">
+          <div className="full-width-image" style={{backgroundImage: `url(${!!image.childImageSharp ? image.childImageSharp.fluid.src : image})`}}>
+            <h1 style={{color: '#ffffff', fontSize: '36pt'}}>STREAMS LIST</h1>
+          </div>
+        </section>
       </div>
     </section>
   </div>
 )
 
 IndexPageTemplate.propTypes = {
-  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+  image: PropTypes.object,
   title: PropTypes.string,
-  heading: PropTypes.string,
-  subheading: PropTypes.string,
+  subtitle: PropTypes.string,
   mainpitch: PropTypes.object,
-  description: PropTypes.string,
-  intro: PropTypes.shape({
-    blurbs: PropTypes.array,
-  }),
+  main: PropTypes.object,
 }
 
 const IndexPage = ({ data }) => {
@@ -134,11 +89,9 @@ const IndexPage = ({ data }) => {
       <IndexPageTemplate
         image={frontmatter.image}
         title={frontmatter.title}
-        heading={frontmatter.heading}
-        subheading={frontmatter.subheading}
+        subtitle={frontmatter.subtitle}
         mainpitch={frontmatter.mainpitch}
-        description={frontmatter.description}
-        intro={frontmatter.intro}
+        main={frontmatter.main}
       />
     </Layout>
   )
@@ -159,6 +112,7 @@ export const pageQuery = graphql`
     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
       frontmatter {
         title
+        subtitle
         image {
           childImageSharp {
             fluid(maxWidth: 2048, quality: 100) {
@@ -166,26 +120,44 @@ export const pageQuery = graphql`
             }
           }
         }
-        heading
-        subheading
         mainpitch {
           title
           description
         }
-        description
-        intro {
-          blurbs {
+        main {
+          heading
+          description
+
+          image1 {
+            alt
             image {
               childImageSharp {
-                fluid(maxWidth: 240, quality: 64) {
+                fluid(maxWidth: 2048, quality: 100) {
                   ...GatsbyImageSharpFluid
                 }
               }
             }
-            text
           }
-          heading
-          description
+          image2 {
+            alt
+            image {
+              childImageSharp {
+                fluid(maxWidth: 2048, quality: 100) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+          image3 {
+            alt
+            image {
+              childImageSharp {
+                fluid(maxWidth: 2048, quality: 100) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
         }
       }
     }
