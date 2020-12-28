d703adba8e2a0ce5156433727186dbc08e449b18
Ryan Amalfitano <ryan.amalfitano@gmail.com>
2020-12-28 17:14:14 -0500

    Update thanks.js
    Improved Thanks page content and styling.

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import Layout from '../../components/Layout'
+import styled from 'styled-components'
+
+const StyledThanksSection = styled.section`
+  text-align: center;
+`
 
 export default () => (
   <Layout>
-    <section className="section">
+    <StyledThanksSection className="section">
       <div className="container">
         <div className="content">
           <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
+          <p>We've got your message and we'll respond as soon as possible.</p>
         </div>
       </div>
-    </section>
+    </StyledThanksSection>
   </Layout>
 )
