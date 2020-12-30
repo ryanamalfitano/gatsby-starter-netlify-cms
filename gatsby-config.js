6bfff4c28563331b44ba4c19eae26df1187a2b32
Ryan Amalfitano <ryan.amalfitano@gmail.com>
2020-12-30 15:06:33 -0500

    Add Video Embed, Remove Twitch Source

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,16 +56,25 @@ module.exports = {
               destinationDir: 'static',
             },
           },
+          {
+            resolve: "gatsby-remark-embed-video",
+            options: {
+              width: 800,
+              ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
+              related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
+              noIframeBorder: true, //Optional: Disable insertion of <style> border: 0
+              urlOverrides: [
+                {
+                  id: 'youtube',
+                  embedURL: (videoId) => `https://www.youtube-nocookie.com/embed/${videoId}`,
+                }
+              ], //Optional: Override URL of a service provider, e.g to enable youtube-nocookie support
+              containerClass: 'embedVideo-container', //Optional: Custom CSS class for iframe container, for multiple classes separate them by space
+            }
+          },
         ],
       },
     },
-    /*{
-      resolve: `gatsby-source-twitch`,
-      options: {
-        userID: '<<Twitch UserID eg. 6058227 >>',
-        clientID: '<< Add your Twitch client_id here>>'
-      },
-    },*/
     {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
