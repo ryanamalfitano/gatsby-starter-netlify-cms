defa0813a87114afaec92b1717835656c4510a43
Ryan Amalfitano <ryan.amalfitano@gmail.com>
2020-12-30 16:20:10 -0500

    Convert Homepage to Markdown HTML Template

--- src/pages/index.md
+++ src/pages/index.md
@@ -1,23 +1,87 @@
 ---
 templateKey: index-page
+path: /
 title: Pixel By Pixel Studios
-subtitle: Home of Stream Interactive Gaming
-image: /img/home-jumbotron.jpg
-mainpitch:
-  title: Welcome to Our Site
-  description: >
-    Some body copy goes here, exploring the studio's goals, values, etc. Probably should also mention Marbles by name as the current flagship product
-main:
-  heading: Why Stream Interactive Gaming?
-  description: >
-
-  image1:
-    alt: Provide a Fresh Experience for Viewers
-    image: /img/home-main1.jpg
-  image2:
-    alt: Increase Audience Engagement
-    image: /img/home-main2.jpg
-  image3:
-    alt: Be Part of an Awesome Community
-    image: /img/home-main3.jpg
 ---
+
+<div class="columns masthead">
+
+<div id="titles" class="column is-6">
+
+<h1 class="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen">
+Pixel By Pixel Studios
+</h1>
+
+<h3 class="is-size-5-mobile is-size-5-tablet is-size-4-widescreen">
+Home of Stream Interactive Gaming
+</h3>
+
+</div>
+
+<div id="stream" class="column is-6 full-width-image margin-top-0">
+
+`twitchLive: mos_pachinko&parent=pixelbypixelstudios.netlify.app&parent=localhost`
+
+</div>
+
+</div>
+
+<div class="content">
+
+<section class="mainPitch">
+
+<h1 class="title">Welcome to Our Site</h1>
+
+<p class="subtitle">Some body copy goes here, exploring the studio's goals, values, etc. Probably should also mention Marbles by name as the current flagship product</p>
+
+</section>
+
+<section class="downloads columns">
+
+<div class="streamer column is-3">
+<h2>For Streamers</h2>
+<a href="https://store.steampowered.com/app/1170970/Marbles_on_Stream/" target="_blank" rel="noreferrer noopener">
+<img src="" alt="Download on Steam" />
+</a>
+</div>
+
+<div class="viewer column is-3">
+<h2>For Viewers</h2>
+<a href="https://apps.apple.com/de/app/marbles-on-stream-mobile/id1443250176" target="_blank" rel="noreferrer noopener">
+<img src="" alt="Download on App Store" />
+</a>
+<a href="https://play.google.com/store/apps/details?id=com.pixelbypixel.mosmobile" target="_blank" rel="noreferrer noopener">
+<img src="" alt="Download on Google Play" />
+</a>
+</div>
+
+</section>
+
+<section class="sellingPoints">
+
+<h3 class="has-text-weight-semibold is-size-2">Why Stream Interactive Gaming?</h3>
+
+<div class="columns">
+
+<div class="column is-4">
+<img src="/img/home-main1.jpg" alt="Provide a Fresh Experience for Viewers" />
+<p>Provide a Fresh Experience for Viewers</p>
+</div>
+
+<div class="column is-4">
+<img src="/img/home-main2.jpg" alt="Increase Audience Engagement" />
+<p>Increase Audience Engagement</p>
+</div>
+
+<div class="column is-4">
+<img src="/img/home-main3.jpg" alt="Be Part of an Awesome Community" />
+<p>Be Part of an Awesome Community</p>
+</div>
+
+</div>
+
+</section>
+
+</div>
+
+</StyledContent>
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -4,8 +4,9 @@ import { graphql } from 'gatsby'
 import styled from 'styled-components'
 
 import Layout from '../components/Layout'
+import Content, { HTMLContent } from '../components/Content'
 
-const StyledMasthead = styled.div`
+const StyledHomePageContent = styled.section`
   #titles {
     margin: auto;
     text-align: right;
@@ -16,10 +17,7 @@ const StyledMasthead = styled.div`
   #stream {
 
   }
-`
 
-const StyledContent = styled.section`
-  
   .container {
     .mainPitch {
       text-align: center;
@@ -60,108 +58,40 @@ const StyledContent = styled.section`
   }
 `
 
-export const IndexPageTemplate = ({
-  image,
-  title,
-  subtitle,
-  mainpitch,
-  main,
-}) => (
-  <div>
-    <StyledMasthead className="columns">
-      <div id="titles" className="column is-6">
-        <h1 className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen">
-          {title}
-        </h1>
-        <h3 className="is-size-5-mobile is-size-5-tablet is-size-4-widescreen">
-          {subtitle}
-        </h3>
-      </div>
-      <div id="stream" className="column is-6 full-width-image margin-top-0"></div>
-    </StyledMasthead>
-    <StyledContent>
-      <div className="container">
-        <section className="mainPitch">
-          <h1 className="title">{mainpitch.title}</h1>
-          <p className="subtitle">{mainpitch.description}</p>
-        </section>
-        <section className="downloads columns">
-          <div className="streamer column is-3">
-            <h2>For Streamers</h2>
-            <a href="https://store.steampowered.com/app/1170970/Marbles_on_Stream/" target="_blank" rel="noreferrer noopener">
-              <img src="" alt="Download on Steam" />
-            </a>
-          </div>
-          <div className="viewer column is-3">
-            <h2>For Viewers</h2>
-            <a href="https://apps.apple.com/de/app/marbles-on-stream-mobile/id1443250176" target="_blank" rel="noreferrer noopener">
-              <img src="" alt="Download on App Store" />
-            </a>
-            <a href="https://play.google.com/store/apps/details?id=com.pixelbypixel.mosmobile" target="_blank" rel="noreferrer noopener">
-              <img src="" alt="Download on Google Play" />
-            </a>
-          </div>
-        </section>
-        <section className="sellingPoints">
-          <h3 className="has-text-weight-semibold is-size-2">
-            {main.heading}
-          </h3>
-          <p>{main.description}</p>
-          <div className="columns">
-            <div className="column is-4">
-              <img src={!!main.image1.image.childImageSharp ? main.image1.image.childImageSharp.fluid.src : main.image1.image} alt={main.image1.alt} />
-              <p>{main.image1.alt}</p>
-            </div>
-            <div className="column is-4">
-              <img src={!!main.image2.image.childImageSharp ? main.image2.image.childImageSharp.fluid.src : main.image2.image} alt={main.image2.alt} />
-              <p>{main.image2.alt}</p>
-            </div>
-            <div className="column is-4">
-              <img src={!!main.image3.image.childImageSharp ? main.image3.image.childImageSharp.fluid.src : main.image3.image} alt={main.image3.alt} />
-              <p>{main.image3.alt}</p>
-            </div>
-          </div>
-        </section>
-     {/*<section className="streamsCarousel full-width-image-container">
-          <div className="full-width-image" style={{backgroundImage: `url(${!!image.childImageSharp ? image.childImageSharp.fluid.src : image})`}}>
-            <h1 style={{color: '#ffffff', fontSize: '36pt'}}>STREAMS LIST</h1>
-          </div>
-        </section>*/}
-      </div>
-    </StyledContent>
-  </div>
-)
+export const IndexPageTemplate = ({title, content, contentComponent}) => {
+  const PageContent = contentComponent || Content
+
+  return (
+  <StyledHomePageContent className="section">
+    <div className="container">
+      <PageContent className="content" content={content} />
+    </div>
+  </StyledHomePageContent>
+  )
+}
 
 IndexPageTemplate.propTypes = {
-  image: PropTypes.object,
   title: PropTypes.string,
-  subtitle: PropTypes.string,
-  mainpitch: PropTypes.object,
-  main: PropTypes.object,
+  content: PropTypes.string,
+  contentComponent: PropTypes.func,
 }
 
 const IndexPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark
+  const { markdownRemark: page } = data
 
   return (
     <Layout>
       <IndexPageTemplate
-        image={frontmatter.image}
-        title={frontmatter.title}
-        subtitle={frontmatter.subtitle}
-        mainpitch={frontmatter.mainpitch}
-        main={frontmatter.main}
+        contentComponent={HTMLContent}
+        title={page.frontmatter.title}
+        content={page.html}
       />
     </Layout>
   )
 }
 
 IndexPage.propTypes = {
-  data: PropTypes.shape({
-    markdownRemark: PropTypes.shape({
-      frontmatter: PropTypes.object,
-    }),
-  }),
+  data: PropTypes.object.isRequired,
 }
 
 export default IndexPage
@@ -169,55 +99,9 @@ export default IndexPage
 export const pageQuery = graphql`
   query IndexPageTemplate {
     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+      html
       frontmatter {
         title
-        subtitle
-        image {
-          childImageSharp {
-            fluid(maxWidth: 2048, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-        mainpitch {
-          title
-          description
-        }
-        main {
-          heading
-          description
-
-          image1 {
-            alt
-            image {
-              childImageSharp {
-                fluid(maxWidth: 2048, quality: 100) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-          }
-          image2 {
-            alt
-            image {
-              childImageSharp {
-                fluid(maxWidth: 2048, quality: 100) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-          }
-          image3 {
-            alt
-            image {
-              childImageSharp {
-                fluid(maxWidth: 2048, quality: 100) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-          }
-        }
       }
     }
   }
