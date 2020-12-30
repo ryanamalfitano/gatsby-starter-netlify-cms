7e47517acef3ceb6b2416622e4c88743dae77854
Ryan Amalfitano <ryan.amalfitano@gmail.com>
2020-12-30 16:19:10 -0500

    Disable gatsby-remark-relative-images

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,12 +35,12 @@ module.exports = {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
-          {
+          /*{
             resolve: 'gatsby-remark-relative-images',
             options: {
               name: 'uploads',
             },
-          },
+          },*/
           {
             resolve: 'gatsby-remark-images',
             options: {
