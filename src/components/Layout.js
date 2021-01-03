542aee2d8bbdc9d814830f184a3df9a71dfc3b06
Ryan Amalfitano <ryan.amalfitano@gmail.com>
2021-01-02 21:02:37 -0500

    Update Layout.js
    Swapped in PBP favicons

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -107,24 +107,24 @@ const TemplateWrapper = ({ children }) => {
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href={`${withPrefix('/')}img/apple-touch-icon.png`}
+          href={`${withPrefix('/')}img/PBPLogo_White.png`}
         />
         <link
           rel="icon"
           type="image/png"
-          href={`${withPrefix('/')}img/favicon-32x32.png`}
+          href={`${withPrefix('/')}img/PBPLogo_White.png`}
           sizes="32x32"
         />
         <link
           rel="icon"
           type="image/png"
-          href={`${withPrefix('/')}img/favicon-16x16.png`}
+          href={`${withPrefix('/')}img/PBPLogo_White.png`}
           sizes="16x16"
         />
 
         <link
           rel="mask-icon"
-          href={`${withPrefix('/')}img/safari-pinned-tab.svg`}
+          href={`${withPrefix('/')}img/PBPLogo_White.png`}
           color="#ff4400"
         />
         <meta name="theme-color" content="#fff" />
@@ -134,7 +134,7 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:url" content="/" />
         <meta
           property="og:image"
-          content={`${withPrefix('/')}img/og-image.jpg`}
+          content={`${withPrefix('/')}img/PBPLogo_White.png`}
         />
       </Helmet>
       <ThemeButton onThemeChange={handleThemeChange} currentTheme={lightingIsDark} />
